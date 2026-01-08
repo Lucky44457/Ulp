9069c32d2ed962fc38bd6984799d4cb4a7a8d58c
Lucky44457 <maribelwate@gmail.com>
2026-01-08 17:10:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
-# Bot configuration
-API_ID = YOUR_API_ID
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+import os
 
-# Command prefixes
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 COMMAND_PREFIX = [",", ".", "/", "!"]
 
-# Developer details
-DEVELOPER_ID = YOUR_USER_ID  # Replace with the developer's actual user ID
-UPDATES_CHANNEL = "YOUR_CHANNEL_URL"  # URL of the updates channel
+DEVELOPER_ID = int(os.getenv("DEVELOPER_ID"))
+UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL")
 
-ADMIN_IDS = [FIRST_ADMIN_ID, SECOND_ADMIN_ID]
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split()))
 
-# MongoDB configuration
-MONGO_URL = "YOUR_MONGO_URL"
+MONGO_URL = os.getenv("MONGO_URL")
